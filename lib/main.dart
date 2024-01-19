a9e11674a3f36f9cb22b41e1b4044b458db2968f
henilzadafiya <henilzadafiya8584@gmail.com>
2024-01-19 11:57:20 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:picture_match/game_paly.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
